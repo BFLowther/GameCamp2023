9ac5424b853393b7585e130a6b8cdecb4bd5cba9
Jack Gruman <jackhotcheese@gmail.com>
2023-07-18 17:42:05 -0500

    Good jumping and movement!

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -7,21 +7,34 @@ public class move : MonoBehaviour
     public int speed;
     public int jumpforce;
     public Rigidbody2D rigi;
-
+    public Vector2 boxSize;
+    public float maxDistance;
+    public LayerMask layerMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-rigi=GetComponent<Rigidbody2D>();   
+        rigi=GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         rigi.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed,rigi.velocity.y);
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            rigi.AddForce(Vector2.up *jumpforce, ForceMode2D.Impulse);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && GroundCheck()) {
+            rigi.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
         }
     }
+
+    private bool GroundCheck() {
+    if(Physics2D.BoxCast(transform.position,boxSize,0,-transform.up,maxDistance,layerMask)) {
+
+    return true;
+
+} else {
+    return false;
+}
+
+
+    } 
 }
