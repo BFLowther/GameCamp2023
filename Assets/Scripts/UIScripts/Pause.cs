88ed8d5700dc21b500fe35914090a0b97391828a
BrockSTaylo <brockt2006@gmail.com>
2023-07-19 17:18:32 -0500

    PauseMenu

---
+++ Assets/Scripts/UIScripts/Pause.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    bool isPaused = false;
+    public GameObject pausePanel;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && isPaused == false){
+            PauseGame();
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused == true){
+            Resume();
+        }
+    }
+
+    public void Quit(){
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+//evan
+    public void PauseGame(){
+        Time.timeScale = 0;
+        isPaused = true;
+        pausePanel.SetActive(true);
+    }
+
+    public void Resume(){
+        Time.timeScale = 1;
+        isPaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    public void BackToStart(){
+        SceneManager.LoadScene("StartScreen");
+    }
+}
