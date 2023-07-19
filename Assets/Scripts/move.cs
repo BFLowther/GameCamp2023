556395072d62b213ac358a34b1ecac7674f68c72
Jack Gruman <jackhotcheese@gmail.com>
2023-07-19 17:39:06 -0500

    crappy animations

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -11,16 +11,25 @@ public class Move : MonoBehaviour
     public float maxDistance;
     public LayerMask layerMask;
 
+
+
+Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        rigi=GetComponent<Rigidbody2D>();   
+        rigi=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigi.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed,rigi.velocity.y);
+        if (Input.GetAxisRaw("Horizontal") == 0.0f) {
+            anim.SetBool("Running", false);
+        } else {
+            rigi.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed,rigi.velocity.y);
+            anim.SetBool("Running", true);
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow) && GroundCheck()) {
             rigi.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
         }
