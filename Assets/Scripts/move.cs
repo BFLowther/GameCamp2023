16443efaca39f16d0b68febdb9b47b81441d4d89
Jack Gruman <jackhotcheese@gmail.com>
2023-07-20 17:49:45 -0500

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -7,12 +7,18 @@ public class Move : MonoBehaviour
     public int speed;
     public int jumpforce;
     public Rigidbody2D rigi;
+
     public Vector2 boxSize;
+    public Vector2 boxSizeRight;
+    public Vector2 boxSizeLeft;
+    public Vector2 boxSizeUp;
+
     public float maxDistance;
     public LayerMask layerMask;
     public SpriteRenderer Player;
     public bool rightFacing;
     public bool jumpedLastFrame;
+    public Vector2 boxCastPosition;
 
 Animator anim;
     // Start is called before the first frame update
@@ -49,14 +55,27 @@ Animator anim;
     }
 
     void FixedUpdate() {
+        boxCastPosition = new Vector2 (transform.position.x,transform.position.y-1);
         if (jumpedLastFrame) {
             rigi.AddForce(Vector2.up * jumpforce * Time.fixedDeltaTime, ForceMode2D.Impulse);
-            jumpedLastFrame = false;
+            jumpedLastFrame = false; }
+        if (wallCheckCeiling() && rigi.velocity.y > 0) {
+            rigi.AddForce(-Vector2.up, ForceMode2D.Impulse);
         }
-    }
 
+        }
+    
     private bool GroundCheck() {
-        if(Physics2D.BoxCast(transform.position,boxSize,0,-transform.up,maxDistance,layerMask)) {
+        if(Physics2D.BoxCast(boxCastPosition,boxSize,0,-transform.up,maxDistance,layerMask)) {
+            return true;
+        } else {
+            return false;
+        } 
+        
+    } 
+    
+    private bool wallCheckCeiling() {
+        if(Physics2D.BoxCast(transform.position,boxSizeUp,0,transform.up,maxDistance,layerMask)) {
             return true;
         } else {
             return false;
