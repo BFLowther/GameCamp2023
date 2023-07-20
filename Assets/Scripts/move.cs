c9dd3a922a8b7dc23b8ae7f4b4e1f415974ab012
Jack Gruman <jackhotcheese@gmail.com>
2023-07-20 15:19:38 -0500

    Changed Player Prefab

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -10,8 +10,9 @@ public class Move : MonoBehaviour
     public Vector2 boxSize;
     public float maxDistance;
     public LayerMask layerMask;
-
-
+    public SpriteRenderer Player;
+    public bool rightFacing;
+    public bool jumpedLastFrame;
 
 Animator anim;
     // Start is called before the first frame update
@@ -19,31 +20,46 @@ Animator anim;
     {
         rigi=GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
+        jumpedLastFrame = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.deltaTime == 0) {
+            return;
+        }
+        if (Input.GetAxis("Horizontal") < 0.0f) {
+            Player.flipX = true;
+        }
+        if (Input.GetAxis("Horizontal") > 0.0f) {
+            Player.flipX = false;
+        }
+
+
         if (Input.GetAxisRaw("Horizontal") == 0.0f) {
             anim.SetBool("Running", false);
         } else {
             rigi.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed,rigi.velocity.y);
             anim.SetBool("Running", true);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && GroundCheck()) {
-            rigi.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+        if ((Input.GetAxis("Jump") > 0 && jumpedLastFrame == false) && GroundCheck()) {
+            jumpedLastFrame = true;
+        }
+    }
+
+    void FixedUpdate() {
+        if (jumpedLastFrame) {
+            rigi.AddForce(Vector2.up * jumpforce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            jumpedLastFrame = false;
         }
     }
 
     private bool GroundCheck() {
-    if(Physics2D.BoxCast(transform.position,boxSize,0,-transform.up,maxDistance,layerMask)) {
-
-    return true;
-
-} else {
-    return false;
-}
-
-
+        if(Physics2D.BoxCast(transform.position,boxSize,0,-transform.up,maxDistance,layerMask)) {
+            return true;
+        } else {
+            return false;
+        }
     } 
 }
