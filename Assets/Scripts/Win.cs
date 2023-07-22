b52b8c6c1d3b5f815b5d77ea05788eb7c3196cc4
Logan <Loganhobson31@gmail.com>
2023-07-21 20:23:12 -0500

    Win animation

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using SuperPupSystems.Helper;
 
 public class Win : MonoBehaviour
 {
     public GameObject winText;
+    public GameObject playerWin;
+    public SpriteRenderer player;
+    public SpriteRenderer prin;
+    public Move move;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Test");
         if(other.gameObject.CompareTag("Player"))
         {
             winText.SetActive(true);
-            Debug.Log("Win");
-            SceneManager.LoadScene("StartScreen");
+            playerWin.SetActive(true);
+            player.enabled = false;
+            move.enabled = false;
+            prin.enabled = false;
+
+            gameObject.GetComponent<Timer>().StartTimer();
         }
+    }
+
+    public void LoadMainScene() {
+        SceneManager.LoadScene("StartScreen");
     }
 }
