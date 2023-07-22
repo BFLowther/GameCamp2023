fb29ed17417d0138d054e10583f2fe9b774eb419
Logan <Loganhobson31@gmail.com>
2023-07-21 20:12:15 -0500

    damage cool down

--- Assets/Scripts/damageOnContact.cs
+++ Assets/Scripts/damageOnContact.cs
@@ -8,11 +8,33 @@ public class DamageOnContact : MonoBehaviour
 {
     public int damage;
     public bool destroyAfterHit = false;
+    public bool damagePause = false;
+    public float coolDown = 0.2f;
+    public float currentCoolDown = 0.0f;
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        currentCoolDown -= Time.deltaTime;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (damagePause || currentCoolDown >= 0.0f)
+            return;
+        
         if (other.gameObject.GetComponent<Health>()) {
             Debug.Log("damaged");
+            currentCoolDown = coolDown;
             Health h = other.gameObject.GetComponent<Health>();
             h.Damage(damage);
             if (destroyAfterHit){
@@ -21,6 +43,5 @@ public class DamageOnContact : MonoBehaviour
         }else {
 
         }
-        
     }
 }
